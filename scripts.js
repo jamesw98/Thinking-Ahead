09f62bfbd11471fd89eb68c7ef3b2a445447f941
jamesw98 <jamesw98@vt.edu>
2019-10-31 13:21:42 -0400

    added blacksburg page

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@
 function gobblerSearch(){
     const base_url = "https://gobblerconnect.vt.edu/organizations?query=";
     var raw_seach = document.getElementById("search").value;
+    console.log(raw_seach);
     window.open(base_url.concat(raw_seach.split(' ').join("%20")), "_blank");
 }
